078929b7cf1e71c00fd40ab098c75b287f877a5d
Awais Saeed <awaissaeed530@gmail.com>
2023-08-10 08:07:24 +0500

    accept tcp connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{net::{TcpListener, TcpStream}, io::Read};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 128];
+    let bytes_read = stream.read(&mut buf).unwrap();
+    println!("{:?}", stream);
+    println!("{:?}", bytes_read);
+    println!("{:?}", String::from_utf8(buf.to_vec()).unwrap());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+
+    for stream in listener.incoming() {
+        handle_client(stream.unwrap());
+    }
 }
